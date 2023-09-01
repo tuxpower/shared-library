116c1d199c63c12658c732f3d3aa678a5e1cecf3
Jose Gaspar <jad.gaspar@gmail.com>
2023-09-01 19:19:24 +0100

    test

--- vars/example.groovy
+++ vars/example.groovy
@@ -2,7 +2,7 @@ import com.cloudbees.plugins.credentials.CredentialsStore
 
 def call() {
 
-  CredentialsStore cs
+  CredentialsStore cs = new CredentialsStore()
   println cs.getDisplayName()
   /*for (c in cs) {
     println "username: ${c.getDisplayName()}"
