921e751f9338d2fa89d9811d4b5fba8d3b6fce28
Jose Gaspar <jad.gaspar@gmail.com>
2023-09-02 09:07:28 +0100

    test

--- vars/credentials.groovy
+++ vars/credentials.groovy
@@ -1,21 +1,33 @@
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider.StoreImpl
 
 def call() {
+    StoreImpl store = new StoreImpl()
 
-  StoreImpl store = new StoreImpl()
-  domains = store.getDomains()
-  for (domain in domains) {
-    if (domain.getName() != null) {
-      creds = store.getCredentials(domain)
-      //println "username: ${c.getName()}"
-      /*if (c instanceof com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl) {
-        //println creds
-        echo 'oooooo'
-      }*/
-      for (c in creds) {
-        println "${c.getUsername()}"
-      }
+    // get list of all domains/folders
+    domains = store.getDomains()
+    for (domain in domains) {
+        // if domain is global doesn't have a name
+        //if (domain.getName() != null) {
+
+        println "Name: ${domain.getName()}"
+        creds = store.getCredentials(domain)
+        for (c in creds) {
+            if (c instanceof com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl) {
+                println "Credentials of type Username/Password"
+                println "ID: ${c.getId()}"
+                println "Username: ${c.getUsername()}"
+                println "Password: ${c.getPassword()}"
+            }
+            
+            if (c instanceof com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey) {
+                println "Credentials of type User/Private Key"
+                println "ID: ${c.getId()}"
+                println "Username: ${c.getUsername()}"
+                println "Passphrase: ${c.getPassphrase()}"
+                println "Privatekey: ${c.getPrivateKey()}"
+            }
+        }
     }
-  }
 }
+
 
